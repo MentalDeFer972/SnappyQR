3990ec7dfd0439f29026ff7c76d74cc864112cd0
MentalDeFer <joffreyterrine@protonmail.com>
2022-11-15 12:08:26 +0100

    commit5

--- app/src/main/java/com/mentaldefer/snappyqr/MainActivity.java
+++ app/src/main/java/com/mentaldefer/snappyqr/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
-    Timestamp timestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
