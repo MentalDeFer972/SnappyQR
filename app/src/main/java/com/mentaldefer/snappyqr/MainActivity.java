26d064d5af46cf15ebd2782a13a6816f37eadace
mentaldefer <joffreyterrine@protonmail.com>
2023-02-20 14:34:09 +0100

    commit6

--- app/src/main/java/com/mentaldefer/snappyqr/MainActivity.java
+++ app/src/main/java/com/mentaldefer/snappyqr/MainActivity.java
@@ -2,10 +2,12 @@ package com.mentaldefer.snappyqr;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
@@ -44,24 +46,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
         String value = result.getContents().toString();
 
-        if (value != null){
-            int year = Integer.parseInt(value.substring(6,9));
-            int mounth = Integer.parseInt(value.substring(3,4));
-            int date = Integer.parseInt(value.substring(0,1));
+        if (value != null) {
+            int year = Integer.parseInt(value.substring(6, 9));
+            int mounth = Integer.parseInt(value.substring(3, 4));
+            int date = Integer.parseInt(value.substring(0, 1));
 
             Calendar calendar = Calendar.getInstance();
-            calendar.set(year,mounth,date);
+            calendar.set(year, mounth, date);
 
             ContentResolver cr = getContentResolver();
             ContentValues values = new ContentValues();
-            values.put(CalendarContract.Events.TITLE,"SnappyQR Events");
-            values.put(CalendarContract.Events.DTSTART,calendar.getTimeInMillis());
+            values.put(CalendarContract.Events.TITLE, "SnappyQR Events");
+            values.put(CalendarContract.Events.DTSTART, calendar.getTimeInMillis());
 
-            Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI,values);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
         }
     }
 }
